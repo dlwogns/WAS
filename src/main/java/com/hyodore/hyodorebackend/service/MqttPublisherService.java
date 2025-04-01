624912e7cdf1121799421e181ce8fa4ece5fa8b4
leejaehun <ldyjka@gmail.com>
2025-04-01 18:22:54 +0900

    feat: Add Mqtt publish feature, using integration MQTT and mosquitto

---
+++ src/main/java/com/hyodore/hyodorebackend/service/MqttPublisherService.java
@@ -0,0 +1,22 @@
+package com.hyodore.hyodorebackend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MqttPublisherService {
+
+    private final MessageChannel mqttOutboundChannel;
+
+    public void publish(String topic, String payload) {
+        Message<String> message = MessageBuilder.withPayload(payload)
+                .setHeader(MqttHeaders.TOPIC, topic)
+                .build();
+        mqttOutboundChannel.send(message);
+    }
+}
