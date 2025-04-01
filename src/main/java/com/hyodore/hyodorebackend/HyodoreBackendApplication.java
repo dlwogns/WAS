bdd3655faca8ff450be24dc2425612d557dabdf7
leejaehun <ldyjka@gmail.com>
2025-04-01 14:47:55 +0900

    Initial commit

---
+++ src/main/java/com/hyodore/hyodorebackend/HyodoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hyodore.hyodorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyodoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyodoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyodore/hyodorebackend/HyodoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyodore.hyodorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyodoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
