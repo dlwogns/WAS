e4fa52cd7172ac4532901fb37ce2f3d5799f2405
leejaehun <ldyjka@gmail.com>
2025-05-25 16:08:27 +0900

    feat(master): 테스트용 API 엔드포인트 생성

--- src/main/java/com/hyodore/hyodorebackend/controller/EventController.java
+++ src/main/java/com/hyodore/hyodorebackend/controller/EventController.java
@@ -3,6 +3,8 @@ package com.hyodore.hyodorebackend.controller;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.hyodore.hyodorebackend.dto.EventRequestDto;
 import com.hyodore.hyodorebackend.service.EventService;
+import com.hyodore.hyodorebackend.service.FCMService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,12 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EventController {
   private final EventService eventService;
+  private final FCMService fcmService;
 
   @PostMapping
   public ResponseEntity<String> receiveEvent(@RequestBody EventRequestDto dto)
       throws FirebaseMessagingException {
     eventService.handleEvent(dto);
     return ResponseEntity.ok("이벤트 처리 완료");
+  }
+
+  @PostMapping("/test1")
+  public ResponseEntity<String> testApi() throws FirebaseMessagingException {
+    fcmService.sendNotification("이벤트발생!", "낙상감지!", Map.of("eventCode", "E1"));
+    return ResponseEntity.ok("굿");
+  }
+
+  @PostMapping("/test2")
+  public ResponseEntity<String> test2Api() throws FirebaseMessagingException {
+    fcmService.sendNotification("이벤트 동영상 수신", "동영상!", Map.of("videoUrl", "url", "userId", "user123"));
+    return ResponseEntity.ok("굿");
   }
 
 }
